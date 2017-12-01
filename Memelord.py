4f86032dfe6f214f364fc8cd24873af5cfd01ba1
Xiyangyang1026 <32981461+Xiyangyang1026@users.noreply.github.com>
2017-11-30 20:34:12 -0500

    Update Memelord.py

--- Memelord.py
+++ Memelord.py
@@ -19,98 +19,119 @@ memes = ["Finger food isn't fingers they said, you just eat it with your fingers
 ,"https://cdn.discordapp.com/attachments/370727786865754117/377500021894938624/tenor.gif"
 ,"https://cdn.discordapp.com/attachments/356163365825150977/374571601192026123/c2wAAAABJRU5ErkJggg.png"
 ,"https://giphy.com/gifs/u--je-P4mzY1dEJg8Ew"
-,"https://youtu.be/xMlT_8jm-0E"]
-comp = ["6M HCL","Debbie used to drink in the lab\nBut now she drinks no more\nFor what she thought was H2O\nWas H2SO4","Can we blow something up today?"
+,"https://youtu.be/xMlT_8jm-0E"
+,'https://cdn.discordapp.com/attachments/336614631755743234/384825419628544016/maxresdefault.png']
+comp = ["6M HCL","Don't trust atoms. They make up everything",'18 MH2SO4',"Can we blow something up today?","Did you know that you can cool yourself to -273˚C and still be 0K?"]
+compy = ["Debbie used to drink in the lab\nBut now she drinks no more\nFor what she thought was H2O\nWas H2SO4"
 ,"Debbie used to breathe in the lab\nBut now she breathes no more\nFor what she thought was O2 gas\nHad a higher molar mass"]
 monname = ['The MemoryHead','The goblin','The ChainChomp','Debbie','The Hammer Bro.','The pufferfish','The 6M HCl','The Calc King','The Hacker Bot'
 ,'The Genji main','The pansey','The ghost pirate','Donald Trump','Hillary Clinton','Bernie Sanders','Kim Jong-Un']
-currentmon = monname[random.randrange(0,len(monname))]
-HP = random.randrange(5000,10000)
+currentmon = monname[random.randint(0,len(monname))]
+wont = ['I refuse.',"don't feel like it",'please no',"but EFFORT..."]
+greetings = ['hello!','salutations!','heyo!']
+HP = random.randint(5000,10000)
+owner = '335958571533598721'
 
+        
 @client.event
 async def on_ready():
-	print(discord.__version__)
-	print("Bot Online!")
-	
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
 @client.command(pass_context=True)
 async def play(ctx, game = None):
-    if game is None:
-        game = 'type ,help'
-
-    await client.change_presence(game=discord.Game(name='{0}'.format(game)))
-	
+    if ctx.message.author.id == owner :
+        if game is None:
+            game = 'type ,help'
+        await client.change_presence(game=discord.Game(name='{0}'.format(game)))
+        await client.say("done!")
+    else :
+        await client.say(wont[random.randint(0,4)])
+        
 @client.command(pass_context=True)
 async def luke (ctx):
-	time.sleep(0.3)
-	await client.say(luke_say[random.randrange(0,5)])
-	
-@client.command(pass_context=True)	
+    time.sleep(0.3)
+    await client.say(luke_say[random.randint(0,5)])
+    
+@client.command(pass_context=True)  
 async def billy (ctx):
-	time.sleep(0.3)
-	await client.say(billy_say[random.randrange(0,6)])
-	
-@client.command(pass_context=True)
-async def nou (ctx):
-	for x in range(0, 10):
-		time.sleep(0.3)
-		await client.say("no u")
-
+    time.sleep(0.3)
+    await client.say(billy_say[random.randint(0,6)])
+        
 @client.command(pass_context=True)
 async def kek (ctx):
-	time.sleep(0.3)		
-	await client.say("kekekekekekekekekekekekekekekekekekekekekekekekekekekekekekekekek")
+    time.sleep(0.3)     
+    await client.say("kekekekekekekekekekekekekekekekekekekekekekekekekekekekekekekekek",tts=True)
 
 @client.command(pass_context=True)
 async def gengu (ctx):
-	time.sleep(0.8)		
-	await client.say("7 upvotes")
-	time.sleep(0.8)
-	await client.say("31 elims")
-	time.sleep(0.8)
-	await client.say("and play")
-	time.sleep(1)
-	await client.say("am i a good gengu now")
+    time.sleep(0.8) 
+    await client.say("7 upvotes")
+    time.sleep(0.8)
+    await client.say("31 elims")
+    time.sleep(0.8)
+    await client.say("and play")
+    time.sleep(1)
+    await client.say("am i a good gengu now (yes/no)")
+    ans = await client.wait_for_message(timeout=None, author=ctx.message.author, check=None)
+    if ans.content == "yes" :
+        await client.say("thank you")
+    elif ans.content == "no" :
+        await client.say("screw you")
+    else :
+        await client.say("thats not an answer")
 
 @client.command(pass_context=True)
 async def kai (ctx):
-	time.sleep(0.3)		
-	await client.say("https://cdn.discordapp.com/attachments/360576264769241098/378665756373811201/Screenshot_2017-11-10-14-11-42-1.png")
-	
+    time.sleep(0.3)     
+    await client.say("https://cdn.discordapp.com/attachments/360576264769241098/378665756373811201/Screenshot_2017-11-10-14-11-42-1.png")
+    
 @client.command(pass_context=True)
-@commands.cooldown(1, 3, commands.BucketType.channel)
 async def hit (ctx):
-	global HP
-	global currentmon
-	damage = random.randrange(100,500)
-	HP -= damage
-	if HP > 0 :
-		await client.say(currentmon	+ ' took ' + str(damage) + ' damage and has ' + str(HP) + ' health left.')
-	else :
-		await client.say(currentmon + ' has been defeated!')
-		currentmon = monname[random.randrange(0,len(monname))]
-		await client.say(currentmon + ' approaches!')
-		HP = random.randrange(1000,5000)
-	
+    global HP
+    global currentmon
+    if ctx.message.author.id == owner :
+        damage = random.randrange(500,800)
+    else :
+        damage = random.randrange(100,500)
+    HP -= damage
+    if HP > 0 :
+        await client.say(currentmon + ' took ' + str(damage) + ' damage and has ' + str(HP) + ' health left.')
+    else :
+        await client.say(currentmon + ' has been defeated!')
+        currentmon = monname[random.randint(0,len(monname))]
+        await client.say(currentmon + ' approaches!')
+        HP = random.randint(1000,5000)
+    
 @client.command(pass_context=True)
 async def rice (ctx):
-	for x in range(0, 10):
-		time.sleep(0.5)
-		await client.say(":rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball:")
+    for x in range(0, 10):
+        time.sleep(0.5)
+        await client.say(":rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball::rice_ball:")
 
-@client.command(pass_context=True)	
+@client.command(pass_context=True)
+async def nou (ctx):
+    for x in range(0, 10):
+        time.sleep(0.5)
+        await client.say("no u", tts=True)
+        
+
+@client.command(pass_context=True)  
 async def dave (ctx):
-	time.sleep(0.3)
-	await client.say("Dave is a unibrow frog with no friends")
-		
-@client.command(pass_context=True)	
+    time.sleep(0.3)
+    await client.say("Dave is a unibrow frog with no friends")
+        
+@client.command(pass_context=True)  
 async def anish (ctx):
-	time.sleep(0.3)
-	await client.say("oi yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+    time.sleep(0.3)
+    await client.say("oi yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
 
-@client.command(pass_context=True)	
+@client.command(pass_context=True)
 async def mario (ctx):
-	time.sleep(0.3)
-	await client.say("── ── ── ── ── ── ── ██ ██ ██ ██ ── ██ ██ ██ ── \n── ── ── ── ── ██ ██ ▓▓ ▓▓ ▓▓ ██ ██ ░░ ░░ ░░ ██ \n── ── ── ── ██ ▓▓ ▓▓ ▓▓ ▓▓ ▓▓ ▓▓ ██ ░░ ░░ ░░ ██ \n" \
+    time.sleep(0.3)
+    await client.say("── ── ── ── ── ── ── ██ ██ ██ ██ ── ██ ██ ██ ── \n── ── ── ── ── ██ ██ ▓▓ ▓▓ ▓▓ ██ ██ ░░ ░░ ░░ ██ \n── ── ── ── ██ ▓▓ ▓▓ ▓▓ ▓▓ ▓▓ ▓▓ ██ ░░ ░░ ░░ ██ \n" \
 "── ── ── ██ ▓▓ ▓▓ ▓▓ ██ ██ ██ ██ ██ ██ ░░ ░░ ██ \n── ── ██ ▓▓ ▓▓ ▓▓ ██ ██ ██ ██ ██ ██ ██ ██ ░░ ██ \n── ── ██ ▓▓ ██ ██ ░░ ░░ ░░ ░░ ░░ ░░ ██ ██ ██ ── \n" \
 "── ██ ██ ██ ██ ░░ ░░ ░░ ██ ░░ ██ ░░ ██ ▓▓ ▓▓ ██ \n── ██ ░░ ██ ██ ░░ ░░ ░░ ██ ░░ ██ ░░ ██ ▓▓ ▓▓ ██ \n██ ░░ ░░ ██ ██ ██ ░░ ░░ ░░ ░░ ░░ ░░ ░░ ██ ▓▓ ██ \n" \
 "██ ░░ ░░ ░░ ██ ░░ ░░ ██ ░░ ░░ ░░ ░░ ░░ ██ ▓▓ ██ \n── ██ ░░ ░░ ░░ ░░ ██ ██ ██ ██ ░░ ░░ ██ ██ ██ ── \n── ── ██ ██ ░░ ░░ ░░ ░░ ██ ██ ██ ██ ██ ▓▓ ██ ── \n" \
@@ -119,15 +140,18 @@ async def mario (ctx):
 "── ██ ██ ██ ▓▓ ▓▓ ▓▓ ██ ██ ██ ██ ██ ██ ██ ██ ── \n── ── ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ██ ▓▓ ▓▓ ██ \n── ██ ▓▓ ▓▓ ██ ██ ██ ██ ██ ██ ██ ██ ▓▓ ▓▓ ▓▓ ██ \n" \
 "██ ██ ▓▓ ██ ██ ██ ██ ██ ██ ██ ██ ██ ▓▓ ▓▓ ▓▓ ██ \n██ ▓▓ ▓▓ ██ ██ ██ ██ ██ ██ ██ ██ ██ ▓▓ ▓▓ ▓▓ ██ \n██ ▓▓ ▓▓ ██ ██ ██ ██ ██ ── ── ── ██ ▓▓ ▓▓ ██ ██ \n" \
 "██ ▓▓ ▓▓ ██ ██ ── ── ── ── ── ── ── ██ ██ ██ ── \n── ██ ██ ── ── ── ── ── ── ── ── ── ── ── ── ──﻿")
-	
-@client.command(pass_context=True)	
+    
+@client.command(pass_context=True)  
 async def meme (ctx):
-	time.sleep(0.3)
-	await client.say(memes[random.randrange(0,6)])
-	
-@client.command(pass_context=True)	
+    time.sleep(0.3)
+    await client.say(memes[random.randint(0,7)])
+    
+@client.command(pass_context=True)  
 async def chem (ctx):
-	time.sleep(0.3)
-	await client.say(comp[random.randrange(0,4)])
+    time.sleep(0.3)
+    if random.randint(0,10)==10 :
+        await client.say(compy[random.randint(0,2)])
+    else :
+        await client.say(comp[random.randint(0,5)])
 
 client.run("MzcxMTA2NzMxNTc0Njg5ODAz.DMwzlg.d7auDagAQ05Iv7lCG2HESJg-nWs")
